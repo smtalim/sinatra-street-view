b751f4c724968af7e00228e5a1f17afce46f9cfd
Rahul Jayaraman <recklessrahul@gmail.com>
2013-07-08 18:41:15 +0530

    fetch address, size & return url

--- lib/fetch_google_street_view.rb
+++ lib/fetch_google_street_view.rb
@@ -1,2 +1,23 @@
 class FetchGoogleStreetView
+
+  def initialize params
+    @address = params.fetch :address
+    @size = params.fetch(:size) { "400x400" }
+    @base_url = "http://maps.googleapis.com/maps/api/streetview?sensor=false" 
+  end
+
+  def fetch_image_source
+    @base_url += add_size
+    @base_url += add_address
+    URI.escape(@base_url).to_s
+  end
+
+  private
+  def add_address
+    "&location=" + @address
+  end
+
+  def add_size
+    "&size=" + @size
+  end
 end
--- spec/fetch_google_street_view_spec.rb
+++ spec/fetch_google_street_view_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../../lib/fetch_google_street_view'
+require_relative '../lib/fetch_google_street_view'
 
 describe FetchGoogleStreetView do
   
@@ -8,8 +8,10 @@ describe FetchGoogleStreetView do
   it "should receive an address &
       return a google street view api link" do
     address = "New York, NY 10002, USA"
-    FetchGoogleStreetView.new(address).fetch.
-      should == "http://maps.googleapis.com/maps/api/streetview?size=400x400&location=40.720032,%20-73.988354&fov=90&heading=235&pitch=10&sensor=false"
+    size = "400x400"
+    final_output = "http://maps.googleapis.com/maps/api/streetview?sensor=false&size=" + size + "&location=" + address
+    FetchGoogleStreetView.new(address: address, size: size).fetch_image_source.
+      should == URI.escape(final_output)
   end
 end
 
